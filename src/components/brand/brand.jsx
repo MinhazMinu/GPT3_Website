7aca1545e2c28bfb487d31ccb8db23ab47240cfa
Minhaz <minhazul.taher@gmail.com>
2023-05-07 18:01:25 +0600

    13 brand  compleated

--- src/components/brand/brand.jsx
+++ src/components/brand/brand.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import "./brand.css";
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
 
 const Brand = () => {
     return (
-        <div>
-            <h1>Brand Component</h1>
+        <div className="gpt3__brand section__padding">
+            <div>
+                <img src={google} alt=""></img>
+            </div>
+            <div>
+                <img src={slack} alt=""></img>
+            </div>
+            <div>
+                <img src={atlassian} alt=""></img>
+            </div>
+            <div>
+                <img src={dropbox} alt=""></img>
+            </div>
+            <div>
+                <img src={shopify} alt=""></img>
+            </div>
         </div>
     );
 };
