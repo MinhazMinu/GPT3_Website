49f51535d9d4e309d2d5f81aca290d9f9dd9e378
Minhaz <minhazul.taher@gmail.com>
2023-05-07 12:30:09 +0600

    04 create a single file to inporte all component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Article, Brand, CTA, Feature, Navbar } from "./components";
 
 const App = () => {
     return (
         <div>
             <h1>GPT3</h1>
+            <Article />
         </div>
     );
 };
